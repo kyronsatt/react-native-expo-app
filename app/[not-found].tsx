3eb94076467eef9ab8e90ec15199a7446b547f68
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2023-07-04 00:24:14 -0300

    feat: initial commit with basic configuration and typescript used

---
+++ app/[not-found].tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function NotFoundPage() {
+  return (
+    <View>
+      <Text>Not Found Page</Text>
+    </View>
+  );
+}
---
+++ app/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomePage() {
+  return (
+    <View>
+      <Text>Home Page</Text>
+    </View>
+  );
+}
