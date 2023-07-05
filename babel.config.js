3a046f9543b5a264ed0a6a4c5968fb1046e9a232
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2023-07-05 18:21:08 -0300

    chore: nativewind configured

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = function (api) {
     plugins: [
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
+      "nativewind/babel",
       require.resolve("expo-router/babel"),
       [
         'module-resolver',
