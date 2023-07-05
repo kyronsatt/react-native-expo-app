3077e51c24247e1afe19d6a23aa124ae8f679d07
Rafael Alexandrino <contato.rafaelalexandrino@gmail.com>
2023-07-05 02:11:33 -0300

    chore: configuration files

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,5 @@
+process.env.EXPO_ROUTER_pages_ROOT = '../../src/pages';
+
 module.exports = function (api) {
   api.cache(true);
   return {
@@ -6,6 +8,37 @@ module.exports = function (api) {
       "@babel/plugin-proposal-export-namespace-from",
       "react-native-reanimated/plugin",
       require.resolve("expo-router/babel"),
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          extensions: ['.js', '.ts', '.tsx', '.json'],
+          alias: {
+            // This needs to be mirrored in tsconfig.json 'paths'
+            _pages: './src/pages',
+            _assets: './src/assets',
+            _services: './src/services',
+            _components: './src/components',
+            _atoms: './src/components/atoms',
+            _molecules: './src/components/molecules',
+            _organisms: './src/components/organisms',
+            _contexts: './src/contexts',
+            _hooks: './src/hooks',
+            _utils: './src/utils',
+          },
+        },
+      ],
+      [
+        'module:react-native-dotenv',
+        {
+          moduleName: '@env',
+          path: '.env',
+          blocklist: null,
+          allowlist: null,
+          safe: false,
+          allowUndefined: true,
+        },
+      ],
     ],
   };
 };
